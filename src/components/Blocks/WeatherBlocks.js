b77096ba9b953fbcf1b1cced6a9ccad2c8ad680a
Xavier Mirabelli-Montan <xavier.mm@investisdigital.com>
2022-03-24 13:44:49 +0000

    Moving top-level weather stats to weather block component.

--- src/components/Blocks/WeatherBlocks.js
+++ src/components/Blocks/WeatherBlocks.js
@@ -1,6 +1,44 @@
 import { Box, Grid, Heading, Paragraph } from "theme-ui"
 import d2d from "degrees-to-direction"
 
+export const WeatherBlockTopLevelStats = ({ data, city, country, tempUnit }) => (
+  <Grid columns={[1, null, 3]}  sx={{
+    backgroundColor: `rgba(0, 0, 0, .25)`,
+    alignItems: `center`,
+    border: `1px solid rgba(0, 0, 0, .3)`,
+    boxShadow: `rgb(0 0 0 / 15%) 0px 0px 10px`,
+    px: 5,
+    my: 5,
+    backdropFilter: `blur(5px)`,
+    borderRadius: `10px`
+  }}>
+    <Box>
+      <Paragraph sx={{
+        fontSize: 8
+      }}>{data.temperature.actual} {tempUnit}</Paragraph>
+      <Paragraph as={'small'}>Feels like: {data.temperature.feelsLike} {tempUnit}</Paragraph>
+    </Box>
+    <Grid columns={1} sx={{
+      m: `0 auto`
+    }}>
+      <img src={`https://openweathermap.org/img/wn/${data.summary.icon}@4x.png`} loading="lazy" alt="Weather status icon"/>
+      <Paragraph
+        sx={{
+          pb:3
+        }}
+        variant={'text.centered'}
+      >
+        {data.summary.title}
+      </Paragraph>
+    </Grid>
+    <Heading as={'h1'} sx={{
+      textAlign: `right`
+    }}>
+      {city}, {country}
+    </Heading>
+  </Grid>
+)
+
 export const WeatherBlockTempurature = ({ data, tempUnit }) => (
   <Box>
     <Heading as={"h3"} sx={{
--- src/templates/CityPage.js
+++ src/templates/CityPage.js
@@ -1,15 +1,20 @@
 import { graphql } from "gatsby"
 import { Box, Paragraph, Grid, Heading } from "theme-ui"
-import d2d from 'degrees-to-direction'
-
 import Layout from "../components/Layouts/Layout"
 import Seo from "../components/Seo/Seo"
 import { useEffect, useState } from "react"
 import { darken, transparentize } from "polished"
-import { WeatherBlockClouds, WeatherBlockTempurature, WeatherBlockWind } from "../components/Blocks/WeatherBlocks"
+import {
+  WeatherBlockClouds,
+  WeatherBlockTopLevelStats,
+  WeatherBlockTempurature,
+  WeatherBlockWind
+} from "../components/Blocks/WeatherBlocks"
 
 const CityPage = ({data, pageContext}) => {
   const [imageData, setImageData] = useState('');
+  const city = data.allWeather.getCityByName.name
+  const country = data.allWeather.getCityByName.country
   const weather = data.allWeather.getCityByName.weather
   const tempUnit = pageContext.unit === 'metric' ? '℃' : '℉'
   const speedUnit = pageContext.unit === 'metric' ? 'm/s' : 'mph'
@@ -43,42 +48,9 @@ const CityPage = ({data, pageContext}) => {
       minHeight: `100vh`
     }}>
       <Layout imageData={imageData}>
-        <Seo title={`${data.allWeather.getCityByName.name}, ${data.allWeather.getCityByName.country} Weather`} />
-        <Grid columns={[1, null, 3]}  sx={{
-          backgroundColor: `rgba(0, 0, 0, .25)`,
-          alignItems: `center`,
-          border: `1px solid rgba(0, 0, 0, .3)`,
-          boxShadow: `rgb(0 0 0 / 15%) 0px 0px 10px`,
-          px: 5,
-          my: 5,
-          backdropFilter: `blur(5px)`,
-          borderRadius: `10px`
-        }}>
-          <Box>
-            <Paragraph sx={{
-              fontSize: 8
-            }}>{weather.temperature.actual} {tempUnit}</Paragraph>
-            <Paragraph as={'small'}>Feels like: {weather.temperature.feelsLike} {tempUnit}</Paragraph>
-          </Box>
-          <Grid columns={1} sx={{
-            m: `0 auto`
-          }}>
-            <img src={`https://openweathermap.org/img/wn/${weather.summary.icon}@4x.png`} loading="lazy" />
-            <Paragraph
-              sx={{
-                pb:3
-              }}
-              variant={'text.centered'}
-            >
-              {weather.summary.title}
-            </Paragraph>
-          </Grid>
-          <Heading as={'h1'} sx={{
-            textAlign: `right`
-          }}>
-            {data.allWeather.getCityByName.name}, {data.allWeather.getCityByName.country}
-          </Heading>
-        </Grid>
+        <Seo title={`${city}, ${country} Weather`} />
+
+        <WeatherBlockTopLevelStats data={weather} city={city} country={country} tempUnit={tempUnit}/>
 
         <Box sx={{
           my: 4
